21b84507394fe842d81c2ff238e0fb8d3292a3b4
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-09-06 12:10:53 +0200

    Добавил метод setPassword в BlogUserEntity

--- project/libs/account/blog-user/src/blog-user-module/blog-user.entity.ts
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.entity.ts
@@ -1,4 +1,4 @@
-import { genSalt, hash } from 'bcrypt';
+import { compare, genSalt, hash } from 'bcrypt';
 import { Entity } from '@project/core';
 import { StorableEntity, AuthUser, UserRole} from '@project/core';
 import { SALT_ROUNDS } from './blog-user.constant';
@@ -46,5 +46,9 @@ export class BlogUserEntity extends Entity implements StorableEntity<AuthUser> {
     const salt = await genSalt(SALT_ROUNDS);
     this.passwordHash = await hash(password, salt);
     return this;
+  }
+
+  public async comparePassword(password: string): Promise<boolean> {
+    return compare(password, this.passwordHash);
   }
 }
