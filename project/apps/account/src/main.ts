dffb8134d08bee6df20d36e94caf8249f51883b2
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-09-06 14:14:57 +0200

    Задокументировал REST API в формате Open API.

--- project/apps/account/src/main.ts
+++ project/apps/account/src/main.ts
@@ -1,13 +1,23 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
-import { GLOBAL_PREFIX } from './app/app.const';
+import { GLOBAL_PREFIX, DEFAULT_PORT } from './app/app.const';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(GLOBAL_PREFIX);
-  const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Account Application')
+    .setDescription('The Readme site the account application description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || DEFAULT_PORT;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`);
 }
